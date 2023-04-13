99741e861e196af3c16f4b3f639a4b8d90173ea3
unicode11 <egoraleshin2006@gmail.com>
2023-04-13 03:08:06 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+namespace Script1
+{
+    public sealed class Program : MyGridProgram
+    {
+        //------------BEGIN--------------
+
+        public Program()
+        { }
+
+        public void Main(string args)
+        {
+
+        }
+
+        public void Save()
+        { }
+
+        //------------END--------------
+    }
+}
