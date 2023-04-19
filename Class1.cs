5f60dfb73d5e07e018c671607600fb515ce6baa9
unicode11 <egoraleshin2006@gmail.com>
2023-04-20 00:24:37 +0300

    MODE, COLORS DONE

--- Class1.cs
+++ Class1.cs
@@ -24,28 +24,30 @@ namespace Script1
 {
     public sealed class Program : MyGridProgram
     {
+        
+        bool COLORS = true ; // To modify colors - scroll down until "var" section
+        bool CLOWN  = false ;
 
-        // COPY FROM HERE
+        // SCRIPT START BELOVE
 
 
         Random ra = new Random();
         List<Sandbox.ModAPI.Ingame.IMyThrust> thrusters = new List<Sandbox.ModAPI.Ingame.IMyThrust>();
-        public static void Main()
-        {
-        }
 
-
-        public void Main(string args)        
+        public void Main(string args)
         {
-            // Vars are in Color(RED, GREEN, BLUE, ALPHA(keep at 255)) format)
-            var E       = new Color(70, 104, 166, 255); // exists to overwrite every thruster
-            var RED     = new Color(255, 0, 0, 255);
-            var ORANGE  = new Color(255, 120, 0, 255);
-            var YELLOW  = new Color(255, 255, 0, 255);
-            var GREEN   = new Color(0, 255, 0, 255);
-            var CYAN    = new Color(0, 255, 255, 255);
-            var BLUE    = new Color(0, 0, 255, 255);
-            var PURPLE  = new Color(120, 0, 255, 255);
+            // Vars are in Color(RED, GREEN, BLUE, ALPHA(keep at 255) format)
+            // To add new color - put your color in RGBA format (look one line up), add it as new var, put name in a "color_array"
+            var RED = new Color(255, 0, 0, 255);
+            var ORANGE = new Color(255, 120, 0, 255);
+            var YELLOW = new Color(255, 255, 0, 255);
+            var GREEN = new Color(0, 255, 0, 255);
+            var CYAN = new Color(0, 255, 255, 255);
+            var BLUE = new Color(0, 0, 255, 255);
+            var PURPLE = new Color(120, 0, 255, 255);
+
+            VRageMath.Color[] color_array =
+                { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE };
 
 
 
@@ -53,14 +55,19 @@ namespace Script1
             foreach (Sandbox.ModAPI.Ingame.IMyThrust thrust in thrusters)
             {
 
-                if (thrust.GetValue<Color>("FlameIdleColorOverride") != E) // 
+                if (CLOWN == true)
                 {
                     thrust.SetValue("FlameIdleColorOverride", new Color(ra.Next(1, 256), ra.Next(1, 256), ra.Next(1, 256)));
                 }
-            }                
+
+                if (COLORS == true)
+                {
+                    thrust.SetValue("FlameIdleColorOverride", color_array[ra.Next(0, color_array.Length)]);
+                }
+            }
         }
-
-
-        // TO HERE
     }
 }
+
+
+        // SCRIPT START ABOVE
