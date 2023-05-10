819e89cc6692225ca668b36408292a2d1ec8b28a
unicode11 <egoraleshin2006@gmail.com>
2023-05-10 15:43:49 +0300

    REFORGED CODE

--- Class1.cs
+++ Class1.cs
@@ -11,63 +11,91 @@ using Sandbox.Game.EntityComponents;
 using VRage.Game.Components;
 using VRage.Collections;
 using VRage.Game.ObjectBuilders.Definitions;
-using VRage.Game.ModAPI.Ingame;
 using SpaceEngineers.Game.ModAPI.Ingame;
 using VRage.Utils;
-using Sandbox.ModAPI;
 using DarkVault.RecolorableThrusters;
 using Sandbox.Game.Gui;
 using System.Runtime.CompilerServices;
 using VRage.Library.Utils;
+using VRage.Game.ObjectBuilders.VisualScripting;
+using VRage;
 
 namespace Script1
 {
     public sealed class Program : MyGridProgram
+
     {
-        // To change modes - write "true" on which mode you need to enable and write "false" on what you want disabled
-        bool COLORS = true ; // To modify colors - scroll down until "var" section
-        bool CLOWN  = false ;
-
-        // SCRIPT START BELOVE
-
-
         Random ra = new Random();
-        List<Sandbox.ModAPI.Ingame.IMyThrust> thrusters = new List<Sandbox.ModAPI.Ingame.IMyThrust>();
+        List<IMyThrust> thrusters = new List<IMyThrust>();
 
-        public void Main(string args)
+        private void Clownify()
         {
-            // Vars are in Color(RED, GREEN, BLUE, ALPHA(keep at 255) format)
+            foreach (IMyThrust thrust in thrusters)
+                thrust.SetValue("FlameIdleColorOverride", new Color(ra.Next(1, 256), ra.Next(1, 256), ra.Next(1, 256)));
+            Echo("\nDONE");
+        }
+
+        private void Colorfy()
+        {
+            // Colros are in Color(RED, GREEN, BLUE, ALPHA(keep at 255) format)
             // To add new color - put your color in RGBA format (look one line up), add it as new var, put name in a "color_array"
-            var RED = new Color(255, 0, 0, 255);
-            var ORANGE = new Color(255, 120, 0, 255);
-            var YELLOW = new Color(255, 255, 0, 255);
-            var GREEN = new Color(0, 255, 0, 255);
-            var CYAN = new Color(0, 255, 255, 255);
-            var BLUE = new Color(0, 0, 255, 255);
-            var PURPLE = new Color(120, 0, 255, 255);
+
+            Color RED = new Color(255, 0, 0, 255);
+            Color ORANGE = new Color(255, 120, 0, 255);
+            Color YELLOW = new Color(255, 255, 0, 255);
+            Color GREEN = new Color(0, 255, 0, 255);
+            Color CYAN = new Color(0, 255, 255, 255);
+            Color BLUE = new Color(0, 0, 255, 255);
+            Color PURPLE = new Color(120, 0, 255, 255);
+
 
             VRageMath.Color[] color_array =
-                { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE };
+            { RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE };
 
+            foreach (IMyThrust thrust in thrusters)
+                thrust.SetValue("FlameIdleColorOverride", color_array[ra.Next(0, color_array.Length)]);
+            Echo("\nDONE");
+        }
+        private enum Mode
+        {
+            COLOR,
+            CLOWN
+        }
 
-
+        private void GetThrusters()
+        {
             GridTerminalSystem.GetBlocksOfType(thrusters);
-            foreach (Sandbox.ModAPI.Ingame.IMyThrust thrust in thrusters)
+        }
+
+        public void Main(string argument)
+        {
+            // NO EDIT BELOW THIS LINE !!!
+
+            Runtime.UpdateFrequency = UpdateFrequency.Update100;
+
+            Echo ("Set mode by typing it into \"Argument\" section of Programmable Block.\nAvailable modes: ");
+            foreach (string value in Enum.GetNames(typeof(Mode)))
             {
+                Echo(value);
+            }
 
-                if (CLOWN == true)
-                {
-                    thrust.SetValue("FlameIdleColorOverride", new Color(ra.Next(1, 256), ra.Next(1, 256), ra.Next(1, 256)));
-                }
 
-                if (COLORS == true)
-                {
-                    thrust.SetValue("FlameIdleColorOverride", color_array[ra.Next(0, color_array.Length)]);
-                }
+            GetThrusters();
+            if (argument == (Mode.CLOWN).ToString())
+            {
+                Clownify();
+            }
+
+            if (argument == (Mode.COLOR).ToString())
+            {
+                Colorfy();
             }
         }
+
+
+
+
+
+
     }
 }
-
-
-        // SCRIPT START ABOVE
